325b825037fbbfab84618a738149ded01863022a
djura328 <djura328@yahoo.com>
2018-01-21 22:44:47 -0500

    add route

---
+++ api/route/orders.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+     res.status(200).json({message: 'GET'})
+});
+
+router.post('/', (req, res, next) => {
+     res.status(200).json({message: 'POST'})
+});
+
+module.exports = router;
