b1eafd71e5e8c01720c04a6d93669bb1c086524b
djura328 <djura328@yahoo.com>
2018-01-21 22:45:20 -0500

    add route

--- api/route/products.js
+++ api/route/products.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-     res.status(200).json({message: 'its work'})
+     res.status(200).json({message: 'GET'})
+});
+
+router.post('/', (req, res, next) => {
+     res.status(200).json({message: 'POST'})
+});
+
+router.patch('/:id', (req, res, next) => {
+     res.status(200).json({message: 'Update', id: req.params.id})
+});
+
+router.delete('/:id', (req, res, next) => {
+     res.status(200).json({message: 'Delete', id: req.params.id})
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app =  express();
 
 const produsctsRoute = require('./api/route/products');
+const ordersRoute = require('./api/route/orders');
 
 app.use('/products', produsctsRoute);
+app.use('/orders', ordersRoute);
 
 module.exports = app;
