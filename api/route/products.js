7c335652e8eb8002bf5bd9051269c34fc7004f44
djura328 <djura328@yahoo.com>
2018-01-25 17:27:57 -0500

    add nodemon

--- api/route/products.js
+++ api/route/products.js
@@ -6,6 +6,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+	console.log(req.body)
      res.status(200).json({message: 'POST'})
 });
 
--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app =  express();
 
 const produsctsRoute = require('./api/route/products');
 const ordersRoute = require('./api/route/orders');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.use('/products', produsctsRoute);
 app.use('/orders', ordersRoute);
