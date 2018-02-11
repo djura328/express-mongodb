1df8e17dbdeac2abdee094973438be410ff53be7
nikola <djura328@yahoo.com>
2018-02-11 16:07:31 +0100

    connect with atlas mongo

--- api/route/products.js
+++ api/route/products.js
@@ -1,21 +1,76 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product  = require('../models/product');
 
 router.get('/', (req, res, next) => {
-     res.status(200).json({message: 'GET'})
+    Product.find()
+        .exec()
+        .then(item => {
+            console.log(item);
+            res.status(200).json(item);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
+        });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.findById(id)
+        .exec()
+        .then(item => {
+            console.log(item);
+            res.status(200).json(item);
+        })
+        .catch(err => {
+            res.status(500).json({error: err});
+        });
 });
 
 router.post('/', (req, res, next) => {
-	console.log(req.body)
-     res.status(200).json({message: 'POST'})
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+    product.save().then(result => {
+       console.log(result);
+    }).catch(err => {
+        console.log(err);
+    });
+    res.status(200).json({
+        message: 'POST',
+        createdProduct: product
+    });
 });
 
-router.patch('/:id', (req, res, next) => {
-     res.status(200).json({message: 'Update', id: req.params.id})
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+
+    Product.update({_id: id}, {$set: {name: req.body.name, price: req.body.price}})
+        .exec()
+        .then(item => {
+            res.status(200).json({message: 'Update', product: item});
+        })
+        .catch(err => {
+            console.log('err');
+        });
 });
 
-router.delete('/:id', (req, res, next) => {
-     res.status(200).json({message: 'Delete', id: req.params.id})
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+
+    Product.remove({_id: id})
+        .exec()
+        .then(item => {
+            res.status(200).json({message: 'Removed', product: item})
+        })
+        .catch(err => {
+            res.status(500).json({error: err});
+        });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app =  express();
 const produsctsRoute = require('./api/route/products');
 const ordersRoute = require('./api/route/orders');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//mongoose.connect("mongodb+srv://djura:zxr328@cluster0-skfcg.mongodb.net/test");
+mongoose.connect("mongodb://djura:zxr328@cluster0-shard-00-00-skfcg.mongodb.net:27017,cluster0-shard-00-01-skfcg.mongodb.net:27017,cluster0-shard-00-02-skfcg.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
